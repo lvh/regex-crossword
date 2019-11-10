f2093a259cf67c90530ff4947ada6d37a50bc2a2
Laurens Van Houtven (lvh) <_@lvh.io>
2019-11-10 14:53:49 -0600

    Cleanup logic

--- src/lvh/regex_crossword/logic.clj
+++ src/lvh/regex_crossword/logic.clj
@@ -31,66 +31,44 @@
   (let [n-vars (count lvars)
         lower (-> lower (or 0))
         upper (-> upper (or n-vars) (max n-vars))]
-
     ;; Even though e.g. the empty string matches "A*", we get the lvars from the
     ;; structure of the puzzle, so we know all lvars have to be matched.
-
     ;; Consequence 1: 0 reps only works if there are 0 vars to match.
     (if (zero? n-vars)
       (if (zero? lower) l/succeed l/fail)
       (l/or*
        (for [reps (range (max lower 1) (inc upper))
-             ;; Consequence 2: can't have any leftovers: must match all parts
+             ;; Consequence 2: can't have any leftovers: must match all parts,
+             ;; group size must be a divisor of the number of lvars
              :when (zero? (rem n-vars reps))
              :let [group-size (quot n-vars reps)
                    groups (partition group-size lvars)]]
          (l/and* (map (partial re->goal elem) groups)))))))
 
-#_(l/run 1 [q]
-    (l/and* (regex-goals #"A" [q])))
-
-#_(l/run 1 [q]
-    (l/and* (regex-goals [#"A" #"B"] q)))
-
-#_(l/run 1 [q r]
-    (l/and* (regex-goals #"(A|B)+" [q r])))
-
-#_(l/run 1 [q]
-    (l/== q 'fred))
-
-#_(l/run 1 [q]
-    (l/== q 'fred)
-    (l/== q 'ethel))
-
-(def dom
+#_(def dom
   (->> (range (int \A) (inc (int \Z))) (map char)))
+#_(l/everyg (fn [v] (l/membero v dom)) vars)
 
 (defn solve
-  [{:keys [x-patterns y-patterns n] :as opts}]
-  (let [n-vars (* (count x-patterns) (count y-patterns))
-        vars (repeatedly n-vars l/lvar)
-        rows (partition (count x-patterns) vars)
-        cols (apply mapv vector rows)
-        pattern-goals (mapcat
-                       re->goal
-                       (concat x-patterns y-patterns)
-                       (concat cols rows))]
-    (->> (l/run n [q]
-           (l/== q vars)
-           (l/everyg (fn [v] (l/membero v dom)) vars)
-           (l/and* pattern-goals)))))
+  ([puzzle]
+   (solve puzzle nil))
+  ([{:keys [x-patterns y-patterns] :as puzzle} {:keys [n] :as opts :or {n 10}}]
+   (let [n-vars (* (count x-patterns) (count y-patterns))
+         vars (repeatedly n-vars l/lvar)
+         rows (partition (count x-patterns) vars)
+         cols (apply mapv vector rows)
+         pattern-goals (map
+                        (fn [patterns lvars]
+                          (l/and*
+                           (map #(-> % cre/parse (re->goal lvars)) patterns)))
+                        (concat x-patterns y-patterns)
+                        (concat cols rows))]
+     (->> (l/run n [q]
+            (l/== q vars)
+            (l/and* pattern-goals))))))
 
-#_(solve {:x-patterns [#"A"] :y-patterns [#"A"]})
-#_(solve {:x-patterns [#"X"] :y-patterns [#"X"]})
-#_(solve {:x-patterns [#"A"] :y-patterns [#"A"]} 10)
-#_(solve {:x-patterns [#"A"] :y-patterns [#"A|B"]})
-#_(solve {:x-patterns [#"A" #"B"] :y-patterns [#"A|B"]})
-
-
-#_(partition-by-weights [1 2 1] '(p q r s))
-
-
-
-
-(def dom
-  (->> (range (int \A) (inc (int \Z))) (map char)))
+#_(solve {:x-patterns [["A"]] :y-patterns [["A"]]})
+#_(solve {:x-patterns [["X"]] :y-patterns [["X"]]})
+#_(solve {:x-patterns [["A"]] :y-patterns [["A"]]} 10)
+#_(solve {:x-patterns [["A"]] :y-patterns [["A|B"]]})
+#_(solve {:x-patterns [["A"] ["B"]] :y-patterns [["A|B"]]})
