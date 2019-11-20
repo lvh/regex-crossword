6404b344d99d2a4fe6607cdbc45c4b7a1948cb3c
Laurens Van Houtven (lvh) <_@lvh.io>
2019-11-19 20:37:50 -0600

    Oops

--- test/lvh/regex_crossword/logic_test.clj
+++ test/lvh/regex_crossword/logic_test.clj
@@ -34,66 +34,77 @@
 (t/deftest re->goal-character-tests
   (t/is (= '(\A)
            (l/run* [q]
-             (rcl/re->goal a [q]))))
+             (rcl/re->goal a [q] {}))))
   (t/is (= '(\A)
            (l/run* [q]
              ;; Note: parser introduces unnecessary alternations/concatenations
-             (rcl/re->goal (cre/parse "A") [q])))))
+             (rcl/re->goal (cre/parse "A") [q] {})))))
 
 (t/deftest re->goal-alternation-test
   (t/is (= '(\A \B)
            (l/run* [q]
              ;; "A|B" with the internal concatenations removed
-             (rcl/re->goal (| \A \B) [q]))))
+             (rcl/re->goal (| \A \B) [q] {}))))
 
   (t/is (= '(\A \B \C)
            (l/run* [q]
              ;; "A|B|C" with the internal concatenations removed
-             (rcl/re->goal (| \A \B \C) [q]))))
+             (rcl/re->goal (| \A \B \C) [q] {}))))
 
   (t/is (= '(\A \B)
            (l/run* [q]
              ;; Note: parser introduces unnecessary alternations/concatenations
-             (rcl/re->goal (cre/parse "A|B") [q])))))
+             (rcl/re->goal (cre/parse "A|B") [q] {})))))
 
 (t/deftest re->goal-concatenation-tests
   (t/is (= '((\A \A))
            (l/run* [p q]
-             (rcl/re->goal (|| \A \A) [p q]))))
+             (rcl/re->goal (|| \A \A) [p q] {}))))
   (t/is (= '((\A \A))
            (l/run* [p q]
-             (rcl/re->goal (cre/parse "AA") [p q])))))
+             (rcl/re->goal (cre/parse "AA") [p q] {})))))
 
 (t/deftest re->goal-repetition-tests
   (t/testing "all lvars must get matched in a repetition"
     (t/is (= '(\A)
              (l/run* [p]
-               (rcl/re->goal (* \A) [p]))))
+               (rcl/re->goal (* \A) [p] {}))))
 
     (t/is (= '((\A \A))
              (l/run* [p q]
-               (rcl/re->goal (* \A) [p q]))))
+               (rcl/re->goal (* \A) [p q] {}))))
 
    (t/is (= '((\A \A \A))
              (l/run* [p q r]
-               (rcl/re->goal (* \A) [p q r]))))
+               (rcl/re->goal (* \A) [p q r] {}))))
 
     (t/is (= '((\A \A \A \A))
              (l/run* [p q r s]
-               (rcl/re->goal (* \A) [p q r s]))))
+               (rcl/re->goal (* \A) [p q r s] {}))))
 
     (t/is (= '(\A)
              (l/run* [p]
-               (rcl/re->goal (+ \A) [p]))))
+               (rcl/re->goal (+ \A) [p] {}))))
     (t/is (= '(\A)
              (l/run* [p]
-               (rcl/re->goal (? \A) [p]))))))
+               (rcl/re->goal (? \A) [p] {}))))
+
+    (t/testing "edge cases where repetition has to fit 1 char into 1 lvar"
+      (t/is (= '(\A)
+               (l/run* [p]
+                 (rcl/re->goal (cre/parse "A{1}") [p] {}))))
+      (t/is (= '(\A)
+               (l/run* [p]
+                 (rcl/re->goal (cre/parse "A{1,1}") [p] {}))))
+      (t/is (= '((\B \A))
+               (l/run* [p q]
+                 (rcl/re->goal (cre/parse "BA{1,1}") [p q] {})))))))
 
 (t/deftest re->goal-class-tests
   (t/testing "class-base"
     (t/is (= '(\A)
              (l/run* [p]
-               (rcl/re->goal {:type :class-base :chars #{\A}} [p])))))
+               (rcl/re->goal {:type :class-base :chars #{\A}} [p] {})))))
 
   (t/testing "class-union"
     (t/is (= '(\A)
@@ -101,7 +112,7 @@
                (rcl/re->goal
                 {:type :class-union
                  :elements [{:type :class-base :chars #{\A}}]}
-                [p]))))
+                [p] {}))))
 
     (t/is (= '(\A \B)
              (l/run* [p]
@@ -109,7 +120,7 @@
                 {:type :class-union
                  :elements [{:type :class-base :chars #{\A}}
                             {:type :class-base :chars #{\B}}]}
-                [p])))))
+                [p] {})))))
 
   (t/testing "class-intersection"
     (t/is (= '(\A)
@@ -117,7 +128,7 @@
                (rcl/re->goal
                 {:type :class-intersection
                  :elements [{:type :class-base :chars #{\A}}]}
-                [p]))))
+                [p] {}))))
 
     (t/is (= '(\A)
              (l/run* [p]
@@ -125,7 +136,7 @@
                 {:type :class-intersection
                  :elements [{:type :class-base :chars #{\A}}
                             {:type :class-base :chars #{\A}}]}
-                [p]))))
+                [p] {}))))
 
     (t/is (= '()
              (l/run* [p]
@@ -133,45 +144,63 @@
                 {:type :class-intersection
                  :elements [{:type :class-base :chars #{\A}}
                             {:type :class-base :chars #{\B}}]}
-                [p])))))
+                [p] {})))))
 
   (t/testing "simple examples from parser, no ranges or negation"
     (t/is (= '(\A)
              (l/run* [q]
-               (-> "[A]" cre/parse (rcl/re->goal [q])))))
+               (-> "[A]" cre/parse (rcl/re->goal [q] {})))))
     (t/is (= '(\A \B)
              (l/run* [q]
-               (-> "[AB]" cre/parse (rcl/re->goal [q]))))))
+               (-> "[AB]" cre/parse (rcl/re->goal [q] {}))))))
 
   (t/testing "ranges"
     (t/is (= '(\A \B \C \D \E \F)
              (l/run* [q]
-               (-> "[A-F]" cre/parse (rcl/re->goal [q])))))
+               (-> "[A-F]" cre/parse (rcl/re->goal [q] {})))))
     (t/is (= #{\A \B \C \D \E \F \H \I \J}
              (set
               (l/run* [q]
-                (-> "[A-FH-J]" cre/parse (rcl/re->goal [q])))))))
+                (-> "[A-FH-J]" cre/parse (rcl/re->goal [q] {})))))))
 
   (t/testing "simple classes"
+    (t/is (= '(\0 \1 \2 \3 \4 \5 \6 \7 \8 \9)
+             (l/run* [q]
+               (-> "\\d" cre/parse (rcl/re->goal [q] {})))))
+    (t/is (= '(_0)
+             (l/run* [q]
+               (-> ".*" cre/parse (rcl/re->goal [q] {})))))
+    (t/is (= '(_0)
+             (l/run* [q]
+               (-> "." cre/parse (rcl/re->goal ['q] {})))))
     (t/is (= '(\space)
              (l/run* [q]
-               (-> "\\s" cre/parse (rcl/re->goal [q]))))))
+               (-> "\\s" cre/parse (rcl/re->goal [q] {}))))))
+
+  (t/testing "simple co-classes (\\D, \\S etc)"
+    (t/are [c1 c2] (= '()
+                      (l/run* [q]
+                        (-> c1 cre/parse (rcl/re->goal [q] {}))
+                        (-> c2 cre/parse (rcl/re->goal [q] {}))))
+      "\\w" "\\W"
+      "\\d" "\\D"
+      "\\s" "\\S"))
 
   (t/testing "negation"
     (t/is (= '(\A)
              (l/run* [q]
-               (-> "[ABC]" cre/parse (rcl/re->goal [q]))
-               (-> "[^B-C]" cre/parse (rcl/re->goal [q])))))
+               (-> "[ABC]" cre/parse (rcl/re->goal [q] {}))
+               (-> "[^B-C]" cre/parse (rcl/re->goal [q] {})))))
     (t/is (= '(\G)
              (l/run* [q]
-               (-> "[A-G]" cre/parse (rcl/re->goal [q]))
-               (-> "[^A-F]" cre/parse (rcl/re->goal [q])))))
+               (-> "[A-G]" cre/parse (rcl/re->goal [q] {}))
+               (-> "[^A-F]" cre/parse (rcl/re->goal [q] {})))))
 
     (t/is (= '(\D)
              (l/run* [q]
-               (-> "[A-G]" cre/parse (rcl/re->goal [q]))
-               (-> "[^A-C]" cre/parse (rcl/re->goal [q]))
-               (-> "[^EFG]" cre/parse (rcl/re->goal [q])))))
+               (-> "[A-G]" cre/parse (rcl/re->goal [q] {}))
+               (-> "[^A-C]" cre/parse (rcl/re->goal [q] {}))
+               (-> "[^EFG]" cre/parse (rcl/re->goal [q] {})))))
 
     (t/testing "goals in opposite order"
       ;; This is an interesting test because l/nafc, once used in the
@@ -181,8 +210,8 @@
       ;; that (somehow -- see impl for details).
       (t/is (= '(\G)
                (l/run* [q]
-                 (-> "[^A-F]" cre/parse (rcl/re->goal [q]))
-                 (-> "[A-G]" cre/parse (rcl/re->goal [q]))))))))
+                 (-> "[^A-F]" cre/parse (rcl/re->goal [q] {}))
+                 (-> "[A-G]" cre/parse (rcl/re->goal [q] {}))))))))
 
 (t/deftest enumerate-class-tests
   (let [enum (fn [pattern]
